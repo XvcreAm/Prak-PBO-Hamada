118ddcf58652412ae647b5289370194c026a8e93
XvcreAm <hamadaags2016@gmail.com>
2022-01-23 22:44:36 +0700

    Modul 06

---
+++ src/com/hamada/modul06/modul06Percobaan/InheritDemo.java
@@ -0,0 +1,7 @@
+package com.hamada.modul06.modul06Percobaan;
+
+public class InheritDemo {
+    public static void main(String[] args) { Student Wahyu = new Student();
+    }
+}
+
---
+++ src/com/hamada/modul06/modul06Percobaan/Student.java
@@ -0,0 +1,13 @@
+package com.hamada.modul06.modul06Percobaan;
+
+public class Student extends Person{ public Student()
+{
+
+//super( "Ini Nama", "Ini Alamat");
+
+//super();
+
+//super.name = "Ini Nama"; System.out.println("Inside Student:Constructor");
+}
+}
+
